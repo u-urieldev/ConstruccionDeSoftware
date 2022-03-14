73d9ecf1c8d1cd6b4b64275d0a6939887ede2219
Uriel Aguilar <uaguilardeveloper@gmail.com>
2022-03-13 18:11:55 -0600

    Ejercicio 5 resuelto

--- web/actividad3/js/eventos_dom.js
+++ web/actividad3/js/eventos_dom.js
@@ -1,3 +1,154 @@
+const CSS_COLOR_NAMES = [
+    "AliceBlue",
+    "AntiqueWhite",
+    "Aqua",
+    "Aquamarine",
+    "Azure",
+    "Beige",
+    "Bisque",
+    "Black",
+    "BlanchedAlmond",
+    "Blue",
+    "BlueViolet",
+    "Brown",
+    "BurlyWood",
+    "CadetBlue",
+    "Chartreuse",
+    "Chocolate",
+    "Coral",
+    "CornflowerBlue",
+    "Cornsilk",
+    "Crimson",
+    "Cyan",
+    "DarkBlue",
+    "DarkCyan",
+    "DarkGoldenRod",
+    "DarkGray",
+    "DarkGrey",
+    "DarkGreen",
+    "DarkKhaki",
+    "DarkMagenta",
+    "DarkOliveGreen",
+    "DarkOrange",
+    "DarkOrchid",
+    "DarkRed",
+    "DarkSalmon",
+    "DarkSeaGreen",
+    "DarkSlateBlue",
+    "DarkSlateGray",
+    "DarkSlateGrey",
+    "DarkTurquoise",
+    "DarkViolet",
+    "DeepPink",
+    "DeepSkyBlue",
+    "DimGray",
+    "DimGrey",
+    "DodgerBlue",
+    "FireBrick",
+    "FloralWhite",
+    "ForestGreen",
+    "Fuchsia",
+    "Gainsboro",
+    "GhostWhite",
+    "Gold",
+    "GoldenRod",
+    "Gray",
+    "Grey",
+    "Green",
+    "GreenYellow",
+    "HoneyDew",
+    "HotPink",
+    "IndianRed",
+    "Indigo",
+    "Ivory",
+    "Khaki",
+    "Lavender",
+    "LavenderBlush",
+    "LawnGreen",
+    "LemonChiffon",
+    "LightBlue",
+    "LightCoral",
+    "LightCyan",
+    "LightGoldenRodYellow",
+    "LightGray",
+    "LightGrey",
+    "LightGreen",
+    "LightPink",
+    "LightSalmon",
+    "LightSeaGreen",
+    "LightSkyBlue",
+    "LightSlateGray",
+    "LightSlateGrey",
+    "LightSteelBlue",
+    "LightYellow",
+    "Lime",
+    "LimeGreen",
+    "Linen",
+    "Magenta",
+    "Maroon",
+    "MediumAquaMarine",
+    "MediumBlue",
+    "MediumOrchid",
+    "MediumPurple",
+    "MediumSeaGreen",
+    "MediumSlateBlue",
+    "MediumSpringGreen",
+    "MediumTurquoise",
+    "MediumVioletRed",
+    "MidnightBlue",
+    "MintCream",
+    "MistyRose",
+    "Moccasin",
+    "NavajoWhite",
+    "Navy",
+    "OldLace",
+    "Olive",
+    "OliveDrab",
+    "Orange",
+    "OrangeRed",
+    "Orchid",
+    "PaleGoldenRod",
+    "PaleGreen",
+    "PaleTurquoise",
+    "PaleVioletRed",
+    "PapayaWhip",
+    "PeachPuff",
+    "Peru",
+    "Pink",
+    "Plum",
+    "PowderBlue",
+    "Purple",
+    "RebeccaPurple",
+    "Red",
+    "RosyBrown",
+    "RoyalBlue",
+    "SaddleBrown",
+    "Salmon",
+    "SandyBrown",
+    "SeaGreen",
+    "SeaShell",
+    "Sienna",
+    "Silver",
+    "SkyBlue",
+    "SlateBlue",
+    "SlateGray",
+    "SlateGrey",
+    "Snow",
+    "SpringGreen",
+    "SteelBlue",
+    "Tan",
+    "Teal",
+    "Thistle",
+    "Tomato",
+    "Turquoise",
+    "Violet",
+    "Wheat",
+    "White",
+    "WhiteSmoke",
+    "Yellow",
+    "YellowGreen",
+]
+
 function mouseTrack(){
     let bd = document.querySelector("body")
     bd.addEventListener("mouseover", (event) => {
@@ -88,7 +239,39 @@ function changeContent(){
     pos.replaceChild(node, pos.firstChild)
 }
 
+function addColor(){
+    
+    // Obtenemos la referencia al select del form
+    let options = document.querySelector("#colorSelect")
+
+    // Creamos la opcion del color y le añadimos un color aleatorio
+    let color_add = document.createElement("option")
+    let col_text = document.createTextNode(CSS_COLOR_NAMES[Math.floor(Math.random() * (CSS_COLOR_NAMES.length))])
+    color_add.appendChild(col_text)
+
+    // Lo añadimos a la lista
+    options.appendChild(color_add)
+}
+
+function removeColor(){
+    // Obtenemos la referencia al select del form
+    let options = document.querySelector("#colorSelect")
+
+    // Seleccionamos el elemento a remover de la lista de opciones de forma aleatoria
+    options.removeChild(options[Math.floor(Math.random() * options.length)])
+}
+
+function changeDivColor(){
+    //Obtenemos la referencia a la lista y el div que cambiara de color
+    let list = document.querySelector("#colorSelect")
+    let div = document.querySelector("#divColor")
+
+    //Modificaos el estilo del div de acuerdo al valor del select
+    div.style.border = `2px solid ${list.value}`
+}
+
 function main(){
     mouseTrack()
+    changeDivColor()
 }
 
